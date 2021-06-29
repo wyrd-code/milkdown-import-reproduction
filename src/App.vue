9b4e4ab8a02302b0f396102a7a91619cc2768513
Tomislav Glavaš <tomo.glavas@gmail.com>
2021-06-29 14:04:02 +0200

    Add Milkdown Editor reproduction

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+
+  <Editor />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Editor from './components/Editor.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Editor,
   }
 })
 </script>
---
+++ src/components/Editor.vue
@@ -0,0 +1,26 @@
+<template>
+  <VueEditor editor="editor" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Editor } from '@milkdown/core';
+import { VueEditor, useEditor } from '@milkdown/vue';
+import { commonmark } from '@milkdown/preset-commonmark';
+
+import '@milkdown/theme-nord/lib/theme.css';
+
+export default defineComponent({
+  name: 'Editor',
+  components: {
+    VueEditor,
+  },
+  setup: () => {
+    const editor = useEditor((root) => new Editor({ root }).use(commonmark));
+
+    return {
+      editor,
+    };
+  }
+})
+</script>
